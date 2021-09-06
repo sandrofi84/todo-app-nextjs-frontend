5547b8f088b05b444cc3fd3490bb259df76615e9
Sandro <sandrofillinich@gmail.com>
2021-09-06 16:09:55 +0100

    feat: center logo vertically

--- src/components/Header/HeaderLogo.tsx
+++ src/components/Header/HeaderLogo.tsx
@@ -1,29 +1,22 @@
 import React from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
-import {createStyles, makeStyles, Theme, Button} from '@material-ui/core';
+import {createStyles, makeStyles, Theme, Button, Box} from '@material-ui/core';
 import myLogo from '../../public/my-logo.svg';
 
 const useStyles = makeStyles((theme: Theme) =>
     createStyles({
         root: {
-            display: "inline-block",
             height: "100%",
             color: theme.palette.primary.contrastText,
-            "&:hover": {
-                "&::after": {
-                    content: '"Home"',
-                    position: 'absolute',
-                    transform: "translate(-50px,50px)",
-                    color: "#000",
-                    backgroundColor: "#FFFF00"
-                },
-            },
+        },
+        button: {
+            height: "100%"
         },
         logo: {
             height: "100%",
             filter: "invert()",
-        }
+        },
     })
 );
 
@@ -32,10 +25,12 @@ const HeaderLogo = () => {
 
     return (
         <div className={classes.root}>
-            <Button>
+            <Button className={classes.button}>
                 <Link href="/">
                     <a>
-                        <Image className={classes.logo} src={myLogo} height={50} alt="home" />
+                        <Box display="flex" flexDirection="column" justifyContent="center">
+                            <Image className={classes.logo} src={myLogo} layout="intrinsic" height={50} alt="home" />
+                        </Box>
                     </a>
                 </Link>
             </Button>
