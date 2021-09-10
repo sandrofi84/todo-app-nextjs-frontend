308c36a25d0658dfb7be4c504c091cce76fb058a
Sandro <sandrofillinich@gmail.com>
2021-09-10 16:30:29 +0100

    feat: add static paths to login/ for oauth providers

--- pages/login/[provider].tsx
+++ pages/login/[provider].tsx
@@ -1,6 +1,20 @@
-import Login from '.';
+import Login from ".";
+import { oauthProviders } from "../../src/components/Form/form-oauth-providers";
 
 export default function LoginWithProvider(props) {
+  return <Login {...props} />;
+}
 
-  return <Login {...props}/>
+export function getStaticProps() {
+  return {
+    props: {}
+  };
+}
+
+export function getStaticPaths() {
+  const paths = oauthProviders.map(provider => ({ params: { provider: provider.name } }));
+  return {
+    paths: paths,
+    fallback: false
+  };
 }
